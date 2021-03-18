8f49bac52aaba4aaff3a72b307a1cf91e2b87f06
Rogrir <jakubus118@gmail.com>
2021-03-18 20:21:48 +0100

    Windows Handle Struct

--- src/main.rs
+++ src/main.rs
@@ -12,43 +12,29 @@ use winapi::{
 #[allow(non_snake_case)]
 mod EventHandler;
 #[allow(non_snake_case)]
-mod WinHandle;
-#[allow(non_snake_case)]
-mod windowAlingment;
-
-use windowAlingment::*;
+mod WindowsHandler;
+use WindowsHandler::*;
 
 static archive: &[u8] = include_bytes!("../dupa.rc");
 
 #[allow(non_snake_case)]
 pub fn main() {
     sciter::set_options(sciter::RuntimeOptions::DebugMode(true));
-
-    let windowAlingment = WindowPos::new(
-        getDesktopResolution(),
-        45,
-        15_f32,
-        43_f32,
-        WINDOWALINGMENT::BottomLeft,
-    );
-    let windowHwnd = unsafe {
-        createWindow(windowProc, windowAlingment).expect("Nie udało się stworzyć okna")
+    let windowHandle = unsafe {
+        WinHandler::new(
+            "clipbaord_manager\0".as_bytes(),
+            "Dupa\0".as_bytes(),
+            WindowPos::new(45, 15_f32, 43_f32, WINDOWALINGMENT::TopRight),
+        )
     };
-    unsafe { winuser::AddClipboardFormatListener(windowHwnd) };
-    let mut frame = sciter::Window::attach(windowHwnd as sciter::types::HWINDOW);
+    unsafe { windowHandle.hookClipboardListener() };
+    let mut frame = sciter::Window::attach(windowHandle.getHWND() as sciter::types::HWINDOW);
     let eventcos = EventHandler::EventHandler { root: None };
     frame.event_handler(eventcos);
     frame.archive_handler(archive).unwrap();
     //frame.load_html(binHtml, Some("this://app/index.htm"));
     frame.load_file("this://app/index.htm");
-    unsafe {
-        winuser::ShowWindow(windowHwnd, winuser::SW_SHOW);
-        let mut msg: winuser::MSG = std::mem::zeroed();
-        while winuser::GetMessageA(&mut msg, null_mut(), 0, 0) != 0 {
-            winuser::TranslateMessage(&msg);
-            winuser::DispatchMessageA(&msg);
-        }
-    }
+    unsafe { windowHandle.messageLoop() };
 }
 
 /*extern "system" fn HostCallbackFnc(scn: sciter::types::LPSCITER_CALLBACK_NOTIFICATION, callbackParam: sciter::types::LPVOID) -> u32
