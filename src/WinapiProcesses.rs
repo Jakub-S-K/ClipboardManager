075feb793a77a8c3a3fd1656039ff8e64b6af650
Jakub-S-K <jakub.stanula.kaczka@gmail.com>
2021-03-21 16:28:12 +0100

    DAMEYO THAT'S SOME REAL SHIT

--- src/WinapiProcesses.rs
+++ src/WinapiProcesses.rs
@@ -13,11 +13,23 @@ pub fn isProcessRunning(name: &str) -> bool {
     let snapshot: winnt::HANDLE = unsafe {tlhelp32::CreateToolhelp32Snapshot(tlhelp32::TH32CS_SNAPPROCESS, 0)};
     unsafe {
         if tlhelp32::Process32First(snapshot, &mut entry) == minwindef::TRUE {
-            //if entry.szExeFile == "target.exe" {
-                todo!();
-            //}
+            while tlhelp32::Process32Next(snapshot, &mut entry) != 0 {
+                let u8_cast = std::mem::transmute::<&[i8], &[u8]>(getNameSlice(&entry.szExeFile).unwrap());
+                if name.as_bytes() ==  u8_cast {
+                    return true;
+                }
+                entry.szExeFile = std::mem::zeroed();
+            } 
         }
     }
-    
-    true
+    false
+}
+
+fn getNameSlice(array: &[i8; 260]) -> Option<&[i8]>{
+    for (i, x) in array.iter().enumerate() {
+        if *x == '\0' as i8 {
+            return Some(&array[0..i]);
+        }
+    }
+    return None;
 }
